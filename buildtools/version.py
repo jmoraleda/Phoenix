4e592a5f9505711c83d10e92fe378d2784f2c7e1
Jorge Moraleda <jorge.moraleda@gmail.com>
2024-03-12 19:18:50 -0400

    Add a github action to publish release artifacts to wxPythonMeticy package in pypi every time a release is updated or edited in github. Set up wxPythonMeticy version number

--- buildtools/version.py
+++ buildtools/version.py
@@ -12,7 +12,7 @@
 #----------------------------------------------------------------------
 
 # Master copy of the project name
-PROJECT_NAME     = 'wxPython'
+PROJECT_NAME     = 'wxPythonMeticy'
 
 
 # The version numbers for wxPython are no longer kept in sync with the
@@ -26,7 +26,7 @@ VER_MAJOR        = 4
 VER_MINOR        = 2
 VER_RELEASE      = 2
 
-VER_FLAGS        = "a1"     # wxPython release flags
+VER_FLAGS        = "a1+2024.3.14"     # wxPython release flags
 
 # The VER_FLAGS value is appended to the version number constructed from the
 # first 3 components and should be set according to the following patterns.
